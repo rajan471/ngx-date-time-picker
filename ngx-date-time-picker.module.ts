d7a692733055107fc7b1b6d6b709c96a3139c0af
dale <dale@tampamoves.com>
2017-12-07 14:18:43 +0530

    Buug fixed

--- ngx-date-time-picker.module.ts
+++ ngx-date-time-picker.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { DatePickerComponent } from './date-picker/date-picker.component';
 
